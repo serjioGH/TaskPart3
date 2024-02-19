ae4f20e6c13f78738231692d71bbaa55d779fa28
serj <serdzhan.ahmedov@digitall.com>
2024-02-19 10:55:32 +0200

    error handling formated

--- Cloth.API/Filter/ErrorHandlingFilter.cs
+++ Cloth.API/Filter/ErrorHandlingFilter.cs
@@ -9,39 +9,20 @@ namespace Cloth.API.Filter;
 
 public class ErrorHandlingFilter : IExceptionFilter
 {
+    public static Error HandleException(Exception exception) => exception switch
+    {
+        AutoMapperMappingException => new Error { StatusCode = HttpStatusCode.BadRequest, Message = exception.Message },
+        ArgumentNullException => new Error { StatusCode = HttpStatusCode.BadRequest, Message = exception.Message },
+        InvalidOperationException => new Error { StatusCode = HttpStatusCode.InternalServerError, Message = exception.Message },
+        ItemNotFoundException => new Error { StatusCode = HttpStatusCode.BadRequest, Message = exception.Message },
+        _ => new Error { StatusCode = HttpStatusCode.InternalServerError, Message = "Unexpected error occured." },
+    };
+
     public void OnException(ExceptionContext context)
     {
         var exception = context.Exception;
-        var exceptionType = exception.GetType();
 
-        var error = new Error
-        {
-            StatusCode = HttpStatusCode.InternalServerError,
-            Message = exception.Message
-        };
-
-        if (exceptionType == typeof(AutoMapperMappingException))
-        {
-            //error.Message = "Mapping objects failed.";
-            error.Message = exception.Message;
-            error.StatusCode = HttpStatusCode.BadRequest;
-        }
-        else if (exceptionType == typeof(ArgumentNullException))
-        {
-            error.Message = exception.Message;
-            error.StatusCode = HttpStatusCode.BadRequest;
-        }
-        else if (exceptionType == typeof(InvalidOperationException))
-        {
-            context.Result = new NotFoundResult();
-            error.StatusCode = HttpStatusCode.InternalServerError;
-            error.Message = "Internel server error.";
-        }
-        else if (exceptionType == typeof(ItemNotFoundException))
-        {
-            error.Message = exception.Message;
-            error.StatusCode = HttpStatusCode.BadRequest;
-        }
+        var error = HandleException(exception);
 
         context.Result = new JsonResult(error);
         context.ExceptionHandled = true;
